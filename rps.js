0566c70cc03db4702e6538e38525ec52f9bda275
matthewgani <matthewgani2@gmail.com>
2022-05-21 17:25:31 +0800

    Add buttons, half done with the scoreboard update

--- rps.js
+++ rps.js
@@ -4,7 +4,116 @@ function computerPlay() {
 }
 
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = computerPlay();
+        console.log(playerSelection);
+        result = playRound(playerSelection, computerSelection);
+        updateScoreBoard(result);
+        
+    })
+})
+
+function updateScoreBoard(result) {
+    const container = document.querySelector('container');
+    const header = document.createElement('h1');
+    switch (result) {
+        case "Win":
+            text = "You Win! " + playerSelection + " beats " + computerSelection;
+            break;
+        case "Lose":
+            text = "You Lose! " + playerSelection + " loses to " + computerSelection;
+            break;
+        case "Draw":
+            text = "Draw! Go again!";
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+
+
+function playRound (playerSelection, computerSelection) {
+    let playerWin = false;
+    let draw = false;
+
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "Rock":
+                draw = true;
+                break;
+            case "Scissors":
+                playerWin = true;
+                break;
+            case "Paper":
+                playerWin = false;
+                break;
+            default:
+                break;
+        }
+
+    }
+    else if (playerSelection === 'paper') {
+        switch (computerSelection) {
+            case "Rock":
+                playerWin = true;
+                break;
+            case "Scissors":
+                playerWin = false;
+                break;
+            case "Paper":
+                draw = true;
+                break;
+            default:
+                break;
+        }
+
+    }
+    else if (playerSelection === 'scissors') {
+        switch (computerSelection) {
+            case "Rock":
+                playerWin = false;
+                break;
+            case "Scissors":
+                draw = true;
+                break;
+            case "Paper":
+                playerWin = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (draw) {
+        return 'Draw';
+    }
+    
+
+    if (playerWin) {
+        return [playerWin, "You Win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection];
+    } else {
+        return [playerWin, "You Lose! " + computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)];
+    }
+}
+
+
+
+
+
+
+
+
+
+// unused when change to GUI
 function simulateRound (playerSelection, computerSelection) {
+    console.log(playerSelection)
+    console.log(computerSelection)
     playerSelection = playerSelection.toLowerCase();
     let playerWin = false;
     let draw = true;
@@ -66,12 +175,12 @@ function simulateRound (playerSelection, computerSelection) {
         }
         if (draw) {
             alert("Draw, go again!");
-            playerSelection = prompt("Rock, paper or scissors?");
+            //playerSelection = prompt("Rock, paper or scissors?");
             computerSelection = computerPlay();
         }
         if (invalidChoice) {
             alert("Invalid choice, go again!");
-            playerSelection = prompt("Rock, paper or scissors?");
+            //playerSelection = prompt("Rock, paper or scissors?");
             draw = true;
             computerSelection = computerPlay();
         }
@@ -83,6 +192,8 @@ function simulateRound (playerSelection, computerSelection) {
     }
 }
 
+
+// unused when changing to GUI format.
 function game() {
     let computerSelection;
     let playerScore = 0;
@@ -109,3 +220,7 @@ function game() {
         console.log("Congratulations! You won!!");
     }
 }
+
+
+
+
